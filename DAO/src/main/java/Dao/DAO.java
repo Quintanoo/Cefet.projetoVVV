0b35db4258e4a3a28bacb11795fc9c995495a6aa
João Almeida <92756397+Quintanoo@users.noreply.github.com>
2023-04-23 17:47:18 -0300

    Add files via upload

---
+++ DAO/src/main/java/Dao/DAO.java
@@ -0,0 +1,9 @@
+package Dao;
+
+import java.util.List;
+
+public interface DAO<T> {
+    public T getSingle(Object... chave);
+    public List<T> getList();
+    public List<T> getList(int top);
+}
---
+++ DAO/src/main/java/Dao/DAOException.java
@@ -0,0 +1,20 @@
+package Dao;
+
+public class DAOException extends Exception {
+
+    public DAOException(Throwable cause) {
+        super(cause);
+    }
+
+    public DAOException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DAOException(String message) {
+        super(message);
+    }
+
+    public DAOException() {
+        super();
+    }
+}
